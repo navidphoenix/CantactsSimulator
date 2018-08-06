7c6cbabd532dfd0f1261e18a9bdd433e43f2b275
Navid <navidphoenix@gmail.com>
2018-08-06 19:23:42 +0430

    build model-commit

---
+++ app/src/main/java/com/example/navid/cantactssimulator/Model/Contact.kt
@@ -0,0 +1,16 @@
+package com.example.navid.cantactssimulator.Model
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "contacts")
+data class Contact
+    (
+        @ColumnInfo(name="name") var name:String ,
+        @ColumnInfo(name="tell") var tell:String ,
+        @ColumnInfo(name="email") var email:String ,
+        @ColumnInfo(name="image") var image:String
+    ) {
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name="id") var id:Int=0
+}
